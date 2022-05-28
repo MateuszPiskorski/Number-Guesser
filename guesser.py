14d6a93023f48e0b3f904b5b18d9cb246d7b5e54
MateuszPiskorski <mpiskorski51@gmail.com>
2022-05-28 12:49:30 +0200

    guesser created and tested

---
+++ guesser.py
@@ -0,0 +1,22 @@
+from random import randint
+
+computer_number = randint(1, 100)
+
+
+def guesser(computer_number: int):
+    player_number = input("Guess the number: ")
+
+    try:
+        player_number = int(player_number)
+    except ValueError:
+        return print("It's not a number!"), guesser(computer_number)
+
+    if player_number < computer_number:
+        return print("To small!"), guesser(computer_number)
+    elif player_number > computer_number:
+        return print("To big!"), guesser(computer_number)
+    else:
+        return print("You win!")
+
+
+guesser(computer_number)
